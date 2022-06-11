ea76525d6d233931900d6d07c3849792e3eb2bc8
CCCWS <86645532+CCCWS@users.noreply.github.com>
2022-06-12 00:57:39 +0900

    Create Modal.js

---
+++ Modal.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import "./Modal.css";
+
+function Modal({ setModalOpen, modalOpen }) {
+  useEffect(() => {
+    if (modalOpen) {
+      document.querySelector("body").classList.toggle("not-scroll");
+    }
+  }, [modalOpen]);
+
+  const modalClose = (event) => {
+    if (event.target.className === "modal modal_open") {
+      setModalOpen(false);
+      document.querySelector("body").classList.toggle("not-scroll");
+    }
+  };
+
+  const modalCloseBtn = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div
+      className={[`modal ${modalOpen ? "modal_open" : null}`].join(" ")}
+      onClick={modalClose}
+    >
+      {modalOpen && (
+        <>
+          <button onClick={modalCloseBtn} className="modalCloseBtn">
+            X
+          </button>
+          <div className="item">
+            <div>content</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Modal;
